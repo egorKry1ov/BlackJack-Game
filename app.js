dfebc074c00bdaf68b539012131e7ac5a6fd17fb
egorkrylov <krylov.yegor96@gmail.com>
2022-03-14 14:57:09 -0400

    promp3

--- app.js
+++ app.js
@@ -5,28 +5,111 @@ let scoreContainer = document.querySelector(".score-el")
 
 
 let suits = ["spades", "hearts", "clubs", "diamonds"]
-let values = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"]
+let values = [
+    {
+        card: "A",
+        points: 1,
+    },
+    {
+        card: "2",
+        points: 2,
+    },
+    {
+        card: "3",
+        points: 3,
+    },
+    {
+        card: "4",
+        points: 4,
+    },
+    {
+        card: "5",
+        points: 5,
+    },
+    {
+        card: "6",
+        points: 6,
+    },
+    {
+        card: "7",
+        points: 7,
+    },
+    {
+        card: "8",
+        points: 8,
+    },
+    {
+        card: "9",
+        points: 9,
+    },  
+    {
+        card: "10",
+        points: 10,
+    },
+    {
+        card: "J",
+        points: 10,
+    },
+    {
+        card: "Q",
+        points: 10,
+    },
+    {
+        card: "K",
+        points: 10,
+    },
+]
 let score = 0
 let deck = []
+scorePoints = values.map(value => value.points)
+console.log(scorePoints)
 
 function getDeck (){
     for (let i = 0; i < suits.length; i++) {
-        for (let j = 0; j < values.length; j++) {
-            let cards = `${values[j]} of ${suits[i]}`
-            deck.push(cards)
-        }
+        for(let j = 0; j < scorePoints.length; j++) {
+        let cards = `${scorePoints[j]} of ${suits[i]}`
+                deck.push(cards)
+            }
     }
-    return deck
 }
 
-// function countScore(){
-//     for (let i = 0; i < values.length; i ++){
-        
-//     }
-// }
+function countScore(){
+    if(scorePoints === 1){
+        score +=1
+    } else if(scorePoints === 2) {
+        score +=2
+    } else if(scorePoints === 3) {
+        score +=3
+    }else if(scorePoints === 4) {
+        score +=4
+    }else if(scorePoints === 5) {
+        score +=5
+    }else if(scorePoints === 6) {
+        score +=6
+    }else if(scorePoints === 7) {
+        score +=7
+    }else if(scorePoints === 8) {
+        score +=8
+    }else if(scorePoints === 9) {
+        score +=9
+    }else {
+        score +=10
+    }
+    scoreContainer.innerText = `Score: ${score}`
+    console.log(score)
+}
+
+function winninConditions() {
+    if(score === 21){
+        console.log("You won")
+    } else if(score > 21) {
+        console.log("You lost")
+    } else {
+    }
+}
 
 function getRandomCard (){
-    let card = deck[Math.floor(Math.random()*deck.length)+2]
+    let card = deck[Math.floor(Math.random()*deck.length)]
     let output = document.createElement("div")
     output.innerText = card 
     output.classList.add("card")
@@ -39,13 +122,14 @@ function getRandomCard (){
     }else {
         output.classList.add("spades")
     }
-    output.innerText = card[0]
-    console.log(card[0])
+    output.innerText = card.split(" ")[0]
+    console.log(card)
     cardContainer.appendChild(output)
+    countScore()
+    winninConditions()
 }
 
 getDeck()
-
 
 randomCard.addEventListener("click", getRandomCard)
 // console.log(getRandomCard())
